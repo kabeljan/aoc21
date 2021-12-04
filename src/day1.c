9df63a82dcc0fe648983e9cb4b315bfbc19bb56b
kabeljan <jan.kablitz@gmail.com>
2021-12-04 20:13:42 +0000

    initial commit

---
+++ src/day1.c
@@ -0,0 +1,8 @@
+#include "day1.h"
+
+#include "stdio.h"
+
+void day1_1(void)
+{
+    printf("Hello World!\n");
+}
---
+++ src/day1.h
@@ -0,0 +1,6 @@
+#ifndef DAY1_H
+#define DAY1_H
+
+void day1_1(void);
+
+#endif /* DAY1_H */
