85e7c07faa77f55cca5bb6c88e9863fd5f0018b3
kabeljan <jan.kablitz@gmail.com>
2021-12-04 20:32:54 +0000

    implement part 1 of day 1

--- src/day1.c
+++ src/day1.c
@@ -2,7 +2,22 @@
 
 #include "stdio.h"
 
+#include "day1_data.h"
+
+#define ARRAY_LEN(array) ((int)(sizeof(array) / sizeof(array[0])))
+
 void day1_1(void)
 {
-    printf("Hello World!\n");
+    int prev_val = a_data[0];
+    int inc_cnt = 0;
+    for (int data_idx = 0; data_idx < ARRAY_LEN(a_data); data_idx++)
+    {
+        if (a_data[data_idx] > prev_val)
+        {
+            inc_cnt++;
+        }
+        prev_val = a_data[data_idx];
+    }
+
+    printf("Number of increases: %i\n", inc_cnt);
 }
